add182dbbe14c8febb17ff003702c915c91fb74f
Rakes S <rakeshsrks2580@gmail.com>
2024-08-15 11:20:59 +0530

    selection Sort

--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -1,10 +1,28 @@
-let array = [3, 5, 2, 6, 1, 4];
-for(let i = 0; i < array.length; i++) {
-	for(let j = 0; j < arra.length; j++) {
-		if(i != j && array[i] < array[j]) {
-			let temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-		}
-	}
+function selectionSort(arr) {
+    let n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        // Assume the minimum is the first element
+        let minIndex = i;
+
+        // Find the minimum element in the unsorted part
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap the found minimum element with the first element
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    return arr;
 }
+
+// Example usage:
+let array = [64, 25, 12, 22, 11];
+console.log("Sorted array:", selectionSort(array));
