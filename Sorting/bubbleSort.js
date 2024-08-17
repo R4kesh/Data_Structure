3cef53dd78bb3ed4f9f0e421064095852a0a329f
Rakes S <rakeshsrks2580@gmail.com>
2024-08-17 09:23:51 +0530

    sort

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -1,10 +1,24 @@
-let array = [3, 5, 2, 6, 1, 4];
-for(let i = 0; i < array.length; i++) {
-	for(let j = 0; j < arra.length; j++) {
-		if(i != j && array[i] < array[j]) {
-			let temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
+function bubbleSort(arr) {
+	let n = arr.length;
+	let swapped;
+  
+	do {
+	  swapped = false;
+	  for (let i = 0; i < n - 1; i++) {
+		if (arr[i] > arr[i + 1]) {
+		  // Swap elements
+		  let temp=arr[i]
+		  arr[i]=arr[i+1]
+		  arr[i+1]=temp
+		  swapped = true;
 		}
-	}
-}
+	  }
+	
+	} while (swapped);
+  
+	return arr;
+  }
+  
+  let array = [64, 34, 25, 12, 22, 11, 90];
+  console.log("Sorted array:", bubbleSort(array));
+  
