ea66604d51099780106c96bf34deadcf4df37333
Rakes S <rakeshsrks2580@gmail.com>
2024-08-20 10:07:10 +0530

    insertion optimized

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -1,12 +1,12 @@
 function insertSort(arr) {
     for (let i = 1; i < arr.length; i++) {
-      let nti = arr[i];
+      let num = arr[i];
       let j = i - 1;
-      while (j >= 0 && arr[j] > nti) {
+      while (j >= 0 && arr[j] > num) {
         arr[j + 1] = arr[j];
         j = j - 1;
       }
-      arr[j + 1] = nti;
+      arr[j + 1] = num;
     }
   }
   
